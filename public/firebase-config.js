485bcbb8739e97801875077b7c5bc8edb44aee45
Alan1203TI <167146297+Alan1203TI@users.noreply.github.com>
2026-06-24 15:35:53 -0300

    Delete public directory

--- public/firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBTm3fU3RcMTkG9Z9SHNNHY3iB7M7fDUlg",
-  authDomain: "diariodebordo-618c6.firebaseapp.com",
-  projectId: "diariodebordo-618c6",
-  storageBucket: "diariodebordo-618c6.firebasestorage.app",
-  messagingSenderId: "326158801251",
-  appId: "1:326158801251:web:95cbba2c788e47ceac5b56"
-};
