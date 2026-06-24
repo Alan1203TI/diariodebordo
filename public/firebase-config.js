e3ebaa6399b1bc66cc024b5c5ef0ccd9a9db294e
Alan1203TI <167146297+Alan1203TI@users.noreply.github.com>
2026-06-24 14:05:38 -0300

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,21 +1,8 @@
-// 1) Crie um projeto no Firebase
-// 2) Ative Authentication > Email/senha
-// 3) Ative Firestore Database
-// 4) Cole aqui as configurações do app Web do Firebase
-export const firebaseConfig = {
-  apiKey: "COLE_SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:0000000000000000000000"
-};
-
-// Opcional: envio direto via EmailJS sem PHP/Node. Use apenas se não for usar Cloud Functions.
-// Crie conta em https://www.emailjs.com/ e preencha os dados abaixo.
-export const emailJsConfig = {
-  enabled: false,
-  publicKey: "",
-  serviceId: "",
-  templateId: ""
+const firebaseConfig = {
+  apiKey: "AIzaSyBTm3fU3RcMTkG9Z9SHNNHY3iB7M7fDUlg",
+  authDomain: "diariodebordo-618c6.firebaseapp.com",
+  projectId: "diariodebordo-618c6",
+  storageBucket: "diariodebordo-618c6.firebasestorage.app",
+  messagingSenderId: "326158801251",
+  appId: "1:326158801251:web:95cbba2c788e47ceac5b56"
 };
